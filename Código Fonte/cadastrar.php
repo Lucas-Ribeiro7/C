b42c83a859f2e5ecbeb68eda227b0342592286ed
Lucas Ribeiro <caetanolucasribeiro@gmail.com>
2020-09-26 14:28:35 -0300

    Update

--- Código Fonte/cadastrar.php
+++ Código Fonte/cadastrar.php
@@ -9,14 +9,8 @@
    <body>
        <div class="borda">
             <?php
-            $nome = $_POST['nome'];
-            $sobrenome = $_POST['sobrenome'];
-            //Conexão com o Banco
-            $con = mysqli_connect('localhost', 'root', '', 'cadastro');
-            //Verificando se o $con tá zerado (Não tem)
-            if(!$con){
-                die("Não foi possivel conectar ao MySQL"); //Die finaliza o Script, caso seja verdadeiro do if() acima
-            }
+            include_once 'conexao.php';
+            
             $sql = "INSERT INTO `pessoa` (`id_pessoa`, `nome`, `sobrenome`) VALUES (NULL, '$nome', '$sobrenome');";
             mysqli_query($con, $sql) or die ("ERRO ao tentar cadastrar o Registro"); //or die é uma instrução caso tenha um erro na sintaxe do $sql
             mysqli_close($con);
---
+++ Código Fonte/conexao.php
@@ -0,0 +1,11 @@
+<?php
+    $nome = $_POST['nome'];
+    $sobrenome = $_POST['sobrenome'];
+    //Conexão com o Banco
+    $con = mysqli_connect('localhost', 'root', '', 'cadastro');
+    //Verificando se o $con tá zerado (Não tem)
+    if(!$con){
+        die("Não foi possivel conectar ao MySQL"); //Die finaliza o Script, caso seja verdadeiro do if() acima
+    }
+
+    include_once 'cadastrar.php';
